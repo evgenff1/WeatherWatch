d12005deb8dc88c03ef8cf11a0dbc98eea8ef6a0
evgenff1 <139433911+evgenff1@users.noreply.github.com>
2024-05-22 09:47:27 +0300

    Initial Commit

---
+++ WeatherWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherWatch
+//
+//  Created by Evgeniy Fakhretdinov on 22.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
